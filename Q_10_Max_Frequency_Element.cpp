e29fc1764fe6a8e4a99362be016063d8b4fdd4cd
DarshPatel9222 <darshpatel9222@gmail.com>
2026-03-18 21:09:23 +0530

    Day_10

---
+++ Q_10_Max_Frequency_Element.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,2,3,3,3,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    unordered_map<int,int>freq;
+    int maxFreq=0;
+    int element=arr[0];
+
+    for(int i=0;i<n;i++){
+        freq[arr[i]]++;
+        if(freq[arr[i]]>maxFreq){
+            maxFreq=freq[arr[i]];
+            element=arr[i];
+        }
+    }
+
+    cout<<"Element: "<<element<<endl;
+    cout<<"Frequency: "<<maxFreq<<endl;
+}
