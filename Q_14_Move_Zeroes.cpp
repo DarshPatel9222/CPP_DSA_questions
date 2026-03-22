c7e7c624a06a3d069b81d6c7c0f8cfa9ad2066d8
DarshPatel9222 <darshpatel9222@gmail.com>
2026-03-22 19:14:26 +0530

    Day_14

---
+++ Q_14_Move_Zeroes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={0,1,0,3,12};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int temp=0;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[temp]);
+            temp++;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
