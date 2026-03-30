ad980acc971c27e782cc4eda9187ac6a9e4fa460
DarshPatel9222 <darshpatel9222@gmail.com>
2026-03-30 21:31:02 +0530

    Day_21

---
+++ Q_21_Two_Sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={2,7,11,15};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int target=9;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                cout<<i<<" "<<j;
+                return 0;
+            }
+        }
+    }
+}
