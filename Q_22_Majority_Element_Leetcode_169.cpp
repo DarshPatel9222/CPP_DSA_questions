01917028e96e485aeb48f414aeea325483f0e39c
DarshPatel9222 <darshpatel9222@gmail.com>
2026-03-31 18:42:38 +0530

    Day_22

---
+++ Q_22_Majority_Element_Leetcode_169.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={2,2,1,1,1,2,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0;i<n;i++){
+        int freq = 0;
+
+        for(int j=0;j<n;j++){
+            if(arr[i] == arr[j]){
+                freq++;
+            }
+        }
+
+        if(freq > n/2){
+            cout << "Majority Element: " << arr[i];
+            break;
+        }
+    }
+}
