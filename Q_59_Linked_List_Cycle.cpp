06438b42bba4381f719997bc658ff33802d4f176
DarshPatel9222 <darshpatel9222@gmail.com>
2026-05-13 20:03:55 +0530

    Day_59

--- Q_59_Linked_List_Cycle.cpp
+++ Q_59_Linked_List_Cycle.cpp
@@ -21,7 +21,6 @@ bool hasCycle(Node* head) {
 
     while (temp != NULL) {
 
-        // Check if already visited
         for (int i = 0; i < size; i++) {
 
             if (visited[i] == temp) {
@@ -29,7 +28,6 @@ bool hasCycle(Node* head) {
             }
         }
 
-        // Store current node
         visited[size++] = temp;
 
         temp = temp->next;
@@ -40,13 +38,11 @@ bool hasCycle(Node* head) {
 
 int main() {
 
-    // 1 -> 2 -> 3 -> 4
     Node* head = new Node(1);
     head->next = new Node(2);
     head->next->next = new Node(3);
     head->next->next->next = new Node(4);
 
-    // Create cycle
     head->next->next->next->next = head->next;
 
     if (hasCycle(head))
