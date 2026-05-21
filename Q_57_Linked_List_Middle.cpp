69a31369b00f14109c2c1d2071c13212d8e5067a
DarshPatel9222 <darshpatel9222@gmail.com>
2026-05-21 17:01:54 +0530

    Day_57

--- Q_57_Linked_List_Middle.cpp
+++ Q_57_Linked_List_Middle.cpp
@@ -32,13 +32,13 @@ ListNode* middleNode(ListNode* head) {
 
 int main() {
 
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
+    ListNode* head=new ListNode(1);
+    head->next=new ListNode(2);
+    head->next->next=new ListNode(3);
+    head->next->next->next=new ListNode(4);
+    head->next->next->next->next=new ListNode(5);
 
-    ListNode* ans = middleNode(head);
+    ListNode* ans=middleNode(head);
 
-    cout << ans->val;
+    cout<<ans->val;
 }
